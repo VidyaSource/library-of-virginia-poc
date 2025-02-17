c0ebbaf58f8be36bf270506212e044bb37455e72
neilchaudhuri <neil@vidyasource.com>
2025-02-17 12:03:59 -0500

    Removed message source

--- src/main/kotlin/com/vidyasource/FtpConfiguration.kt
+++ src/main/kotlin/com/vidyasource/FtpConfiguration.kt
@@ -1,28 +1,23 @@
 package com.vidyasource
 
 import org.apache.commons.net.ftp.FTPClient
-import org.apache.commons.net.ftp.FTPFile
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.expression.common.LiteralExpression
-import org.springframework.integration.annotation.InboundChannelAdapter
-import org.springframework.integration.channel.QueueChannel
+import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.dsl.IntegrationFlow
-import org.springframework.integration.dsl.MessageChannels
 import org.springframework.integration.dsl.integrationFlow
-import org.springframework.integration.file.FileHeaders.REMOTE_FILE
 import org.springframework.integration.file.remote.gateway.AbstractRemoteFileOutboundGateway
+import org.springframework.integration.file.support.FileExistsMode
+import org.springframework.integration.ftp.filters.FtpRegexPatternFileListFilter
 import org.springframework.integration.ftp.gateway.FtpOutboundGateway
-import org.springframework.integration.ftp.inbound.FtpStreamingMessageSource
 import org.springframework.integration.ftp.session.DefaultFtpSessionFactory
-import org.springframework.integration.ftp.session.FtpRemoteFileTemplate
 import org.springframework.integration.metadata.PropertiesPersistingMetadataStore
+import org.springframework.integration.support.MessageBuilder
 import org.springframework.messaging.Message
-import java.io.File
-import java.io.InputStream
 
 @ConfigurationProperties(prefix = "ftp")
 data class FtpProperties @ConstructorBinding constructor(
@@ -53,25 +48,26 @@ class FtpConnectionConfiguration(val ftpProperties: FtpProperties) {
         }
     }
 
-    @Bean
-    @InboundChannelAdapter(channel = "inputChannel")
-    fun ftpStreamingMessageSource() = FtpStreamingMessageSource(FtpRemoteFileTemplate(ftpSessionFactory())).apply {
-        maxFetchSize = 20
-        setRemoteDirectory(".")
 
+    @Bean
+    fun inputChannel() = DirectChannel()
+
+    @Bean
+    fun startupRunner(inputChannel: DirectChannel) = ApplicationRunner {
+        println("Triggering FTP mget command...")
+        inputChannel.send(MessageBuilder.withPayload("").build()) // Empty payload to trigger the flow
     }
-
-    @Bean
-    fun inputChannel() = QueueChannel()
 
     @Bean
     fun outboundGateway() = FtpOutboundGateway(ftpSessionFactory(), "mget").apply {
         setLocalDirectoryExpression(LiteralExpression("local/"))
         setOption(AbstractRemoteFileOutboundGateway.Option.RECURSIVE)
+        setFilter(FtpRegexPatternFileListFilter("""^(?!.*\.zip$).*"""))
+        setFileExistsMode(FileExistsMode.APPEND)
     }
 
     @Bean
-    fun integrationFlow(inputChannel: QueueChannel, outboundGateway: FtpOutboundGateway): IntegrationFlow {
+    fun integrationFlow(inputChannel: DirectChannel, outboundGateway: FtpOutboundGateway): IntegrationFlow {
         return integrationFlow(inputChannel) {
             handle(outboundGateway)
             handle { message: Message<*> ->
