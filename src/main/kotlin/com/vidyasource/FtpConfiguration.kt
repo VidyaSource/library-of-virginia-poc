75da345726d234919ed876143849723a3a478c44
neilchaudhuri <neil@vidyasource.com>
2025-02-17 14:03:10 -0500

    Use queue

--- src/main/kotlin/com/vidyasource/FtpConfiguration.kt
+++ src/main/kotlin/com/vidyasource/FtpConfiguration.kt
@@ -1,20 +1,21 @@
 package com.vidyasource
 
+
 import org.apache.commons.net.ftp.FTPClient
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.expression.common.LiteralExpression
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.integrationFlow
 import org.springframework.integration.file.remote.gateway.AbstractRemoteFileOutboundGateway
-import org.springframework.integration.file.support.FileExistsMode
 import org.springframework.integration.ftp.filters.FtpRegexPatternFileListFilter
 import org.springframework.integration.ftp.gateway.FtpOutboundGateway
 import org.springframework.integration.ftp.session.DefaultFtpSessionFactory
+import org.springframework.integration.ftp.session.FtpFileInfo
 import org.springframework.integration.metadata.PropertiesPersistingMetadataStore
 import org.springframework.integration.support.MessageBuilder
 import org.springframework.messaging.Message
@@ -27,6 +28,7 @@ data class FtpProperties @ConstructorBinding constructor(
     val password: String
 )
 
+data class LibraryFile(val fullPathName: String, val timestamp: Long)
 
 @Configuration
 class FtpConnectionConfiguration(val ftpProperties: FtpProperties) {
@@ -53,25 +55,41 @@ class FtpConnectionConfiguration(val ftpProperties: FtpProperties) {
     fun inputChannel() = DirectChannel()
 
     @Bean
-    fun startupRunner(inputChannel: DirectChannel) = ApplicationRunner {
-        println("Triggering FTP mget command...")
+    fun startupRunner(@Qualifier("inputChannel") inputChannel: DirectChannel) = ApplicationRunner {
         inputChannel.send(MessageBuilder.withPayload("").build()) // Empty payload to trigger the flow
     }
 
     @Bean
-    fun outboundGateway() = FtpOutboundGateway(ftpSessionFactory(), "mget").apply {
-        setLocalDirectoryExpression(LiteralExpression("local/"))
+    fun outboundGateway() = FtpOutboundGateway(ftpSessionFactory(), "ls").apply {
+        //setLocalDirectoryExpression(LiteralExpression("local/"))
         setOption(AbstractRemoteFileOutboundGateway.Option.RECURSIVE)
+        //setOption(AbstractRemoteFileOutboundGateway.Option.NAME_ONLY)
         setFilter(FtpRegexPatternFileListFilter("""^(?!.*\.zip$).*"""))
-        setFileExistsMode(FileExistsMode.APPEND)
+        //outputChannel
+        //setFileExistsMode(FileExistsMode.APPEND)
     }
 
+
+    fun fileTransformer(ftpFileInfo: FtpFileInfo): LibraryFile {
+        return LibraryFile(
+            fullPathName = ftpFileInfo.filename,
+            timestamp = ftpFileInfo.modified
+        )
+    }
+
+
     @Bean
-    fun integrationFlow(inputChannel: DirectChannel, outboundGateway: FtpOutboundGateway): IntegrationFlow {
+    fun integrationFlow(inputChannel: DirectChannel,
+                        outboundGateway: FtpOutboundGateway): IntegrationFlow {
         return integrationFlow(inputChannel) {
+            log("starting integration flow")
             handle(outboundGateway)
-            handle { message: Message<*> ->
-                println("Headers: ${message.headers}")
+            split<Message<*>> { it.payload }
+            transform<FtpFileInfo> { fileTransformer(it) }
+            channel { queue(10) }
+            handle { file: Message<*> ->
+                val libraryFile = file.payload as? LibraryFile
+                println("LibraryFile: $libraryFile")
             }
         }
     }
