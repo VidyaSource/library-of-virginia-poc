1b5fdf142ee3b7bf68226156daf262881a4366f3
neilchaudhuri <neil@vidyasource.com>
2025-02-16 20:20:46 -0500

    Experiment with OutboundGateway

--- src/main/kotlin/com/vidyasource/FtpConfiguration.kt
+++ src/main/kotlin/com/vidyasource/FtpConfiguration.kt
@@ -1,27 +1,36 @@
 package com.vidyasource
 
 import org.apache.commons.net.ftp.FTPClient
+import org.apache.commons.net.ftp.FTPFile
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.expression.common.LiteralExpression
 import org.springframework.integration.annotation.InboundChannelAdapter
 import org.springframework.integration.channel.QueueChannel
 import org.springframework.integration.dsl.IntegrationFlow
+import org.springframework.integration.dsl.MessageChannels
 import org.springframework.integration.dsl.integrationFlow
 import org.springframework.integration.file.FileHeaders.REMOTE_FILE
+import org.springframework.integration.file.remote.gateway.AbstractRemoteFileOutboundGateway
+import org.springframework.integration.ftp.gateway.FtpOutboundGateway
 import org.springframework.integration.ftp.inbound.FtpStreamingMessageSource
 import org.springframework.integration.ftp.session.DefaultFtpSessionFactory
 import org.springframework.integration.ftp.session.FtpRemoteFileTemplate
 import org.springframework.integration.metadata.PropertiesPersistingMetadataStore
 import org.springframework.messaging.Message
+import java.io.File
 import java.io.InputStream
 
 @ConfigurationProperties(prefix = "ftp")
-data class FtpProperties @ConstructorBinding constructor(val host: String,
-                         val port: Int,
-                         val username: String,
-                         val password: String)
+data class FtpProperties @ConstructorBinding constructor(
+    val host: String,
+    val port: Int,
+    val username: String,
+    val password: String
+)
 
 
 @Configuration
@@ -56,14 +65,17 @@ class FtpConnectionConfiguration(val ftpProperties: FtpProperties) {
     fun inputChannel() = QueueChannel()
 
     @Bean
-    fun integrationFlow(ftpStreamingMessageSource: FtpStreamingMessageSource): IntegrationFlow {
-        return integrationFlow(ftpStreamingMessageSource, { poller { it.fixedDelay(3).maxMessagesPerPoll(10) } }) {
-            filter<Message<InputStream>> { m ->
-                println("header: ${m.headers[REMOTE_FILE]}")
-                !m.headers[REMOTE_FILE].toString().endsWith(".zip", ignoreCase = true)
-            }
-            handle { message ->
-                println("Found file: ${message}")
+    fun outboundGateway() = FtpOutboundGateway(ftpSessionFactory(), "mget").apply {
+        setLocalDirectoryExpression(LiteralExpression("local/"))
+        setOption(AbstractRemoteFileOutboundGateway.Option.RECURSIVE)
+    }
+
+    @Bean
+    fun integrationFlow(inputChannel: QueueChannel, outboundGateway: FtpOutboundGateway): IntegrationFlow {
+        return integrationFlow(inputChannel) {
+            handle(outboundGateway)
+            handle { message: Message<*> ->
+                println("Headers: ${message.headers}")
             }
         }
     }
